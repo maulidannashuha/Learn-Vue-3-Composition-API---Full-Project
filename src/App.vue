2e230d5a996ead03348f5646c5611271a5a1e238
Maulidan Nashuha <maulidannashuha@gmail.com>
2024-02-12 09:36:22 +0700

    initial commit and sclicing

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpence />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  import Balance from './components/Balance.vue'
+  import IncomeExpence from './components/IncomeExpence.vue'
+  import TransactionList from './components/TransactionList.vue'
+  import AddTransaction from './components/AddTransaction.vue'
+
+  export default{
+    components: {
+      Header,
+      Balance,
+      IncomeExpence,
+      TransactionList,
+      AddTransaction,
+    }
+  }
+</script>
---
+++ src/components/IncomeExpence.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p id="money-plus" class="money plus">asfsd</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p id="money-minus" class="money minus">asfs</p>
+      </div>
+    </div>
+  </template>
